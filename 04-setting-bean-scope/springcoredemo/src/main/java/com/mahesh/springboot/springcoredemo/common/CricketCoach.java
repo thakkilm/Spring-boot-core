c6d48a2e7a706b6bde1127cd3298f4892d935eea
Mahesh <thakkilm@mail.gvsu.edu>
2023-12-03 23:26:34 -0500

    set using default scope

--- 04-setting-bean-scope/springcoredemo/src/main/java/com/mahesh/springboot/springcoredemo/common/CricketCoach.java
+++ 04-setting-bean-scope/springcoredemo/src/main/java/com/mahesh/springboot/springcoredemo/common/CricketCoach.java
@@ -2,10 +2,12 @@ package com.mahesh.springboot.springcoredemo.common;
 
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+//@Scope()
 public class CricketCoach implements Coach {
 
     public  CricketCoach(){
--- 04-setting-bean-scope/springcoredemo/src/main/java/com/mahesh/springboot/springcoredemo/rest/DemoController.java
+++ 04-setting-bean-scope/springcoredemo/src/main/java/com/mahesh/springboot/springcoredemo/rest/DemoController.java
@@ -10,15 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private Coach myCoach;
+    private Coach myAnotherCoach;
     @Autowired
 //    public DemoController(@Qualifier("tennisCoach") Coach myCoach){
 //        this.myCoach=myCoach;
 //    }
-    public DemoController( Coach myCoach){
+    public DemoController( Coach myCoach,Coach myAnotherCoach){
         this.myCoach=myCoach;
+        this.myAnotherCoach=myAnotherCoach;
     }
     @GetMapping("/daily-workout")
     public String getDailyWorkOut(){
+
         return myCoach.getDailyWorkOut();
+    }
+    @GetMapping("/scope-check")
+    public String scopeCheck(){
+
+        return "Both myCoach and myAnotherCoach belongs to same scope: "+(myCoach==myAnotherCoach);
     }
 }
